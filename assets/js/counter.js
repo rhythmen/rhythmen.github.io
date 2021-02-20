e78d9c466ff4f807fc6bda1abb49f4e78ac79db8
rhythmen <79357147+rhythmen@users.noreply.github.com>
2021-02-20 13:06:44 +0300

    Add files via upload

---
+++ assets/js/counter.js
@@ -0,0 +1,48 @@
+var yandex = {
+    "v1c": 60932182,
+    "v2": 60932185,
+    "v3": 60932188,
+    "v4": 60932191,
+    "v5": 64528273,
+    "v6": 64528276,
+    "v7": 64528282,
+    "v8": 64528285,
+    "v9": 64528288,
+    "v10": 64528270,
+};
+
+
+
+function $_GET(key) {
+    var s = window.location.search;
+    s = s.match(new RegExp(key + '=([^&=]+)'));
+    return s ? s[1] : '';
+}
+
+var site = $_GET('version').replace('149', '')
+
+if (!site) {
+    var site = document.location.pathname
+        .replace(/\/+/g, '')
+        .replace('index.html', '')
+        .replace('index2.html', '')
+        .replace('prelp', '')
+        .replace('confirm.html', '')
+        .replace('149', '');
+}
+
+(function (m, e, t, r, i, k, a) {
+    m[i] = m[i] || function () {
+        (m[i].a = m[i].a || []).push(arguments)
+    };
+    m[i].l = 1 * new Date();
+    k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+})
+(window, document, "script", "../https@mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(yandex[site], "init", {
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true
+});
